284a2c98b79d1ed2d4a2b6e4ff53a34b91d48787
JoelRubio99 <joel8867@hotmail.com>
2021-10-11 20:15:33 -0500

    add ProfileCreditCard project

---
+++ ProfileCreditCard/src/main/java/org/academy/jra/ProfileCreditCardApplication.java
@@ -0,0 +1,13 @@
+package org.academy.jra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileCreditCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileCreditCardApplication.class, args);
+	}
+
+}
