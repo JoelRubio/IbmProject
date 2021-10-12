8c7180f287a41e4c1ec8d5f4e5221b972722b628
JoelRubio99 <joel8867@hotmail.com>
2021-10-11 21:15:09 -0500

    add repository files

---
+++ ProfileCreditCard/src/main/java/org/academy/jra/repository/ProfileRepository.java
@@ -0,0 +1,10 @@
+package org.academy.jra.repository;
+
+import org.academy.jra.model.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfileRepository extends JpaRepository<Profile, Long> {
+
+}
